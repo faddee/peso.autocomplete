0584f1e5fa226af5a1f18b6f8c109ff7f881392d
Fredrik Nicol <fredrik.nicol@capgemini.com>
2013-11-25 15:58:18 +0100

    Version 0.2.5

--- peso.autocomplete.js
+++ peso.autocomplete.js
@@ -1,6 +1,6 @@
 /**
  * $.autocomplete, a.k.a. peso.autocomplete
- * v0.2.4
+ * v0.2.5
  * 
  * Lightweight autocompletion with minimal DOM manipulation as possible, supported by jQuery and Zepto.
  * https://github.com/faddee/peso-autocomplete
